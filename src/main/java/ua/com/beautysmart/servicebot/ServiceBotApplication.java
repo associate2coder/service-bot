b3bd4f960b89de33f1a5805437248f630a7b9928
o-lat-ukr <olatsko@gmail.com>
2023-10-04 18:46:55 +0300

    tidy up

--- src/main/java/ua/com/beautysmart/servicebot/ServiceBotApplication.java
+++ src/main/java/ua/com/beautysmart/servicebot/ServiceBotApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceBotApplication {
 
     public static void main(String[] args) {
-        System.out.println(System.getenv().get("JDBC_URL"));
         SpringApplication.run(ServiceBotApplication.class, args);
     }
 
